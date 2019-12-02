35702d22b0d5a0cb2f8422fd54827227e1d84aed
Antonio Carlos Furtado <marcojrfurtado@gmail.com>
2019-12-01 21:36:44 -0500

    Updates

---
+++ precache-manifest.460bee333c51451e08b1259e39a4aa70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f96dd5d7d54996821a39eb4dc9b2ef",
+    "url": "/WaterlooZoneChangeRequests/index.html"
+  },
+  {
+    "revision": "e40d94a8831afcdf39dd",
+    "url": "/WaterlooZoneChangeRequests/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "de702bc4eaa6836e9a41",
+    "url": "/WaterlooZoneChangeRequests/static/js/2.1ec1e84a.chunk.js"
+  },
+  {
+    "revision": "e40d94a8831afcdf39dd",
+    "url": "/WaterlooZoneChangeRequests/static/js/main.dde3d5bd.chunk.js"
+  },
+  {
+    "revision": "0fc6c9d554ca14a38f7a",
+    "url": "/WaterlooZoneChangeRequests/static/js/runtime-main.ec7d220e.js"
+  }
+]);
